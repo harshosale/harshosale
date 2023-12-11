12bf347224158be69b134b552439563133015989
Harshosale <harshad.bhosale@cogoport.com>
2023-12-11 14:31:27 +0530

    image from assets and responsive website

--- src/header.js
+++ src/header.js
@@ -1,11 +1,19 @@
 import styles from "./styles.module.css";
+import header_image from "./header_image.jpeg";
+import { useState } from "react";
 
 const Header = ({ setNavigation = () => {}, options = [] }) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleBarsClick = (val = false) => {
+    setShowMenu((prev) => val && !prev);
+  };
+
   return (
     <div className={styles.header}>
       <img
         className={styles.header_image}
-        src="https://avatars.githubusercontent.com/u/79321559?v=4"
+        src={header_image}
         alt="my pic"
         onClick={() => setNavigation("")}
       />
@@ -13,15 +21,36 @@ const Header = ({ setNavigation = () => {}, options = [] }) => {
         <div className={styles.header_name} onClick={() => setNavigation("")}>
           Harshad Bhosale
         </div>
-        <div className={styles.header_navigations}>
+        <div
+          className={
+            showMenu ? styles.header_navigation_menu : styles.header_navigations
+          }
+        >
           {options.map((option) => (
             <div
               className={styles.header_navigation}
-              onClick={() => setNavigation(option.nav)}
+              onClick={() => {
+                handleBarsClick();
+                setNavigation(option.nav);
+              }}
             >
               {option.name}
             </div>
           ))}
+        </div>
+        <div
+          className={
+            showMenu
+              ? styles.header_navigation_cross
+              : styles.header_navigation_bars
+          }
+          onClick={handleBarsClick}
+        >
+          {[...Array(3).keys()].map(() => {
+            return (
+              <div className={showMenu ? styles.cross : styles.bars}></div>
+            );
+          })}
         </div>
       </div>
     </div>
